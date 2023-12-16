25e6231ba80a5cc67ca0b50ef6669629b5050c6c
mihanizzm <m.sartakov@g.nsu.ru>
2023-12-16 11:48:48 +0700

    added 2-2 2-3

---
+++ 2-2/mutex/queue-mutex.c
@@ -0,0 +1,135 @@
+#include "queue-mutex.h"
+
+void *qmonitor(void *arg) {
+    queue_t *q = (queue_t *)arg;
+
+    printf("qmonitor: [%d %d %lu]\n", getpid(), getppid(), pthread_self());
+
+    while (1) {
+        queue_print_stats(q);
+        sleep(1);
+    }
+
+    return NULL;
+}
+
+queue_t* queue_init(int max_count) {
+    int err;
+
+    queue_t *q = malloc(sizeof(queue_t));
+    if (!q) {
+        printf("Cannot allocate memory for a queue\n");
+        abort();
+    }
+
+    q->first = NULL;
+    q->last = NULL;
+    q->max_count = max_count;
+    q->count = 0;
+
+    // Initialize the mutex
+    err = pthread_mutex_init(&q->mutex, NULL);
+    if (err) {
+        printf("queue_init: pthread_mutex_init() failed: %s\n", strerror(err));
+        abort();
+    }
+
+    q->add_attempts = q->get_attempts = 0;
+    q->add_count = q->get_count = 0;
+
+    err = pthread_create(&q->qmonitor_tid, NULL, qmonitor, q);
+    if (err) {
+        printf("queue_init: pthread_create() failed: %s\n", strerror(err));
+        abort();
+    }
+
+    return q;
+}
+
+void queue_destroy(queue_t *q) {
+    // Stop the qmonitor thread
+    pthread_cancel(q->qmonitor_tid);
+    pthread_join(q->qmonitor_tid, NULL);
+
+    // Destroy the mutex
+    pthread_mutex_destroy(&q->mutex);
+
+    // Free the memory allocated for queue nodes
+    while (q->first != NULL) {
+        qnode_t *tmp = q->first;
+        q->first = q->first->next;
+        free(tmp);
+    }
+
+    // Free the memory allocated for the queue
+    free(q);
+}
+
+int queue_add(queue_t *q, int val) {
+    q->add_attempts++;
+
+    assert(q->count <= q->max_count);
+
+    if (q->count == q->max_count)
+        return 0;
+
+    qnode_t *new = malloc(sizeof(qnode_t));
+    if (!new) {
+        printf("Cannot allocate memory for new node\n");
+        abort();
+    }
+
+    new->val = val;
+    new->next = NULL;
+
+    // Acquire the mutex before modifying the queue
+    pthread_mutex_lock(&q->mutex);
+
+    if (!q->first)
+        q->first = q->last = new;
+    else {
+        q->last->next = new;
+        q->last = q->last->next;
+    }
+
+    q->count++;
+    q->add_count++;
+
+    // Release the mutex after modifying the queue
+    pthread_mutex_unlock(&q->mutex);
+
+    return 1;
+}
+
+int queue_get(queue_t *q, int *val) {
+    q->get_attempts++;
+
+    assert(q->count >= 0);
+
+    if (q->count == 0)
+        return 0;
+
+    // Acquire the mutex before modifying the queue
+    pthread_mutex_lock(&q->mutex);
+
+    qnode_t *tmp = q->first;
+
+    *val = tmp->val;
+    q->first = q->first->next;
+
+    free(tmp);
+    q->count--;
+    q->get_count++;
+
+    // Release the mutex after modifying the queue
+    pthread_mutex_unlock(&q->mutex);
+
+    return 1;
+}
+
+void queue_print_stats(queue_t *q) {
+    printf("queue stats: current size %d; attempts: (%ld %ld %ld); counts (%ld %ld %ld)\n",
+        q->count,
+        q->add_attempts, q->get_attempts, q->add_attempts - q->get_attempts,
+        q->add_count, q->get_count, q->add_count - q->get_count);
+}
---
+++ 2-2/mutex/queue-mutex.h
@@ -0,0 +1,44 @@
+#ifndef __FITOS_QUEUE_MUTEX_H__
+#define __FITOS_QUEUE_MUTEX_H__
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <assert.h>
+
+typedef struct qnode {
+    int val;
+    struct qnode *next;
+} qnode_t;
+
+typedef struct {
+    qnode_t *first;
+    qnode_t *last;
+    int max_count;
+    int count;
+
+    // Replaced spinlock with mutex
+    pthread_mutex_t mutex;
+
+    // Monitor thread information
+    pthread_t qmonitor_tid;
+
+    // Statistics
+    long add_attempts;
+    long get_attempts;
+    long add_count;
+    long get_count;
+} queue_t;
+
+queue_t* queue_init(int max_count);
+void queue_destroy(queue_t *q);
+int queue_add(queue_t *q, int val);
+int queue_get(queue_t *q, int *val);
+void queue_print_stats(queue_t *q);
+
+#endif		// __FITOS_QUEUE_MUTEX_H__
